7d7f76de378e010cf2904e5774dfcaeca721f54d
as6620 <as6620@bs.amalnet.k12.il>
2025-02-24 23:14:05 +0200

    id + screen structure

--- app/src/main/java/com/example/sharedprefrences/MainActivity.java
+++ app/src/main/java/com/example/sharedprefrences/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sharedprefrences;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +13,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText eT;
+    TextView tVNum;
+    Button btnCount, btnReset, btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
+        EditText eT = (EditText) findViewById(R.id.eT);
+        TextView tVNum = (TextView) findViewById(R.id.tVNum);
+        Button btnCount = (Button) findViewById(R.id.btnCount);
+        Button btnReset = (Button) findViewById(R.id.btnReset);
+        Button btnExit = (Button) findViewById(R.id.btnExit);
+    }
+
+    public void goCount(View view) {
+    }
+
+    public void goReset(View view) {
+    }
+
+    public void goExit(View view) {
     }
 }
