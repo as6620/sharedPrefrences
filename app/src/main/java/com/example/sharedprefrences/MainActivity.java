2513a2aed2900d9b5678a2a0c4ca8532fbc6d812
as6620 <as6620@bs.amalnet.k12.il>
2025-03-03 21:01:59 +0200

    credits

--- app/src/main/java/com/example/sharedprefrences/MainActivity.java
+++ app/src/main/java/com/example/sharedprefrences/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.sharedprefrences;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -57,5 +61,22 @@ public class MainActivity extends AppCompatActivity {
         editor.putInt("count",counter);
         editor.commit();
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menuCred) {
+            Intent si = new Intent(this, activity_credits.class);
+            startActivity(si);
+        }
+        return true;
     }
 }
